3ac25d70b913ce35445785d8ea52c28e453c5fab
stfbnc <stefanobianchi9@gmail.com>
2021-05-16 22:04:21 +0200

    tool script; html pages script

---
+++ output_pages/daily_correlations_page.py
@@ -0,0 +1,206 @@
+#  daily_correlations_page.py - this file is part of the gwadaptive_scattering package.
+#  Copyright (C) 2020- Stefano Bianchi
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+import os
+import sys
+from datetime import datetime
+from astropy.time import Time
+from ..html import html_builder as hb
+from ..utils import file_utils
+
+
+#SAVE_PATH = os.path.expandvars("/data/dev/web/detchar/daily_corr/")
+SAVE_PLOTS_FOLDER = "plots"
+SUMMARY_IMFS = 1
+COLOR_THRESHOLD_MIN = 0.5
+COLOR_THRESHOLD = 0.7
+COPY_OR_MOVE = "cp"
+
+
+def daily_correlations_page(res_path, date, tc_name, aux_ch, save_path):
+    """Output page for daily correlations analysis.
+
+    Parameters
+    ----------
+    res_path : str
+        path containing analyses' results folders
+    date : str
+        date to be published on the page (year-month-day)
+    tc_name : str
+        target channel name
+    aux_ch : str
+        auxiliary channel name
+    save_path : str
+        path where to store the page files
+    """
+    res_folders = file_utils.get_results_folders(res_path, must_include=["*.png"])
+
+    # title
+    page_date = datetime.strptime(date, "%Y-%m-%d")
+    curr_date = page_date.strftime('%Y%m%d')
+    curr_folder = os.path.join(save_path, curr_date, "_".join(tc_name.split(":")))
+    curr_plots_folder = os.path.join(curr_folder, SAVE_PLOTS_FOLDER)
+    os.system("mkdir -p {}".format(curr_plots_folder))
+    title = "Scattered light daily analysis ({})".format(page_date.strftime('%Y-%m-%d'))
+    page = hb.HtmlBuilder(title=title, **{"style": "body { background-color: white; }"})
+
+    # info
+    page.addSection("Info")
+    page.openDiv(**{"id_": "info-list"})
+
+    info_dict = {
+        "Environment": page.getFormattedCode(sys.prefix),
+        "Target channel": tc_name,
+        "Auxiliary channel": aux_ch
+    }
+    page.addBulletList(info_dict)
+    page.closeDiv()
+
+    # correlation plot
+    if os.path.exists(os.path.join(res_path, "comparison")):
+        page.addSection("Correlation between {} and {}".format(tc_name, aux_ch))
+
+        for i in range(1, SUMMARY_IMFS + 1):
+            ts_corr_name = os.path.join(res_path, "comparison", "imf_{}_ts_corr.png".format(i))
+            if os.path.exists(ts_corr_name):
+                page.openDiv(**{"id_": "imf-{}-summary".format(i)})
+                page.addSubsection("Imf {}".format(i))
+
+                ts_corr_to_save = os.path.join(curr_plots_folder, "imf-{}-ts-corr.png".format(i))
+
+                os.system("{} {} {}".format(COPY_OR_MOVE, ts_corr_name, ts_corr_to_save))
+                page.addPlot(os.path.join(SAVE_PLOTS_FOLDER, "imf-{}-ts-corr.png".format(i)),
+                             "imf-{}-ts-corr".format(i))
+                page.closeDiv()
+
+    hour_dict = {}
+    for ih in range(24):
+        hour_dict[ih] = []
+
+    for gf in res_folders:
+        gps_times = gf.split(os.path.sep)[-1].split("_")
+        gps_event = (int(gps_times[0]) + int(gps_times[1])) // 2
+        t1 = Time(gps_event, format="gps")
+        t2 = Time(t1, format="iso", scale="utc")
+        h = int("{}".format(t2).split(" ")[1].split(":")[0])
+        hour_dict[h].append(gf)
+
+    # results
+    page.addSection("Single GPS results")
+
+    page.openDiv(**{"id_": "info-color"})
+
+    info_color_dict = {
+        "Yellow": "Correlation between {} and {}".format(COLOR_THRESHOLD_MIN, COLOR_THRESHOLD),
+        "Red": "Correlation greater than {}".format(COLOR_THRESHOLD)
+    }
+    page.addBulletList(info_color_dict)
+    page.closeDiv()
+
+    page.openDiv(**{"id_": "results"})
+
+    for hh in hour_dict.keys():
+        if len(hour_dict[hh]) > 0:
+            date_h = "{} {:02d}:00:00".format(page_date.strftime('%Y-%m-%d'), hh)
+
+            # main card
+            page.openCard(date_h, "primary", "main-card-{:d}".format(hh))
+
+            for gps_path in hour_dict[hh]:
+                res_file = file_utils.YmlFile(gps_path)
+                parameters = [
+                    ("GPS", ",".join([str(g) for g in res_file.get_gps()])),
+                    ("Target channel", res_file.get_target_channel()),
+                    ("Channels list", res_file.get_channels_list()),
+                    ("Output path", res_file.get_output_path()),
+                    ("Sampling frequency", res_file.get_sampling_frequency()),
+                    ("Lowpass frequency", res_file.get_lowpass_frequency()),
+                    ("Scattering factor", res_file.get_scattering_factor()),
+                    ("Smoothing window", res_file.get_smoothing_window())
+                ]
+                imfs_data = {}
+                above_thr = False
+                for i in range(1, SUMMARY_IMFS + 1):
+                    if res_file.get_imfs_count() >= i:
+                        imfs_exists = os.path.exists(os.path.join(gps_path, "imf_{}_culprit.png".format(i)))
+                        if imfs_exists:
+                            imfs_data[i] = {}
+                            imfs_data[i]["Culprit"] = res_file.get_channel_of_imf(i)
+                            imfs_data[i]["Mean frequency"] = "{:.4f} Hz".format(res_file.get_mean_freq_of_imf(i))
+                            imfs_data[i]["omegagram"] = os.path.exists(os.path.join(gps_path,
+                                                                                    "imf_{:d}_omegagram.png".format(i)))
+
+                            if res_file.get_corr_of_imf(i) >= COLOR_THRESHOLD:
+                                above_thr = True
+
+                gps_start, gps_end = res_file.get_gps()
+                gps_event = (gps_start + gps_end) // 2
+                res_id = "{:d}-{:d}".format(gps_start, gps_end)
+                t1 = Time(gps_event, format="gps")
+                t2 = Time(t1, format="iso", scale="utc")
+                gps_date = "{} UTC (GPS: {:d})\n".format(t2, gps_event)
+
+                if above_thr:
+                    color_key = "danger"
+                else:
+                    color_key = "warning"
+
+                # card
+                page.openCard(gps_date, color_key, res_id)
+
+                # parameters table
+                page.parametersTable(parameters, int(gps_start), int(gps_end))
+
+                # plots
+                for i in range(1, SUMMARY_IMFS + 1):
+                    if i in imfs_data.keys():
+                        page.openDiv(**{"id_": "imf-{}-{}".format(i, res_id)})
+                        page.addSubsection("Imf {}".format(i))
+
+                        omegagram = imfs_data[i].pop("omegagram")
+
+                        page.openDiv(**{"id_": "imf-{}-{}-info".format(i, res_id)})
+                        page.addBulletList(imfs_data[i])
+                        page.closeDiv()
+
+                        imf_plot_name = os.path.join(gps_path, "imf_{}_culprit.png".format(i))
+                        imf_to_save = os.path.join(curr_plots_folder, "imf-{}-{}.png".format(i, res_id))
+                        os.system("{} {} {}".format(COPY_OR_MOVE, imf_plot_name, imf_to_save))
+                        page.openDiv(**{"id_": "imf-{}-{}-plot".format(i, res_id)})
+                        page.addPlot(os.path.join(SAVE_PLOTS_FOLDER, "imf-{}-{}.png".format(i, res_id)),
+                                     "imf-{}-{}".format(i, res_id))
+                        page.closeDiv()
+
+                        if omegagram:
+                            omegagram_plot_name = os.path.join(gps_path, "imf_{}_omegagram.png".format(i))
+                            omegagram_to_save = os.path.join(curr_plots_folder, "omegagram-{}-{}.png".format(i, res_id))
+                            os.system("{} {} {}".format(COPY_OR_MOVE, omegagram_plot_name, omegagram_to_save))
+                            page.openDiv(**{"id_": "omegagram-{}-{}-plot".format(i, res_id)})
+                            page.addPlot(os.path.join(SAVE_PLOTS_FOLDER, "omegagram-{}-{}.png".format(i, res_id)),
+                                         "omegagram-{}-{}".format(i, res_id))
+                            page.closeDiv()
+
+                        page.closeDiv()
+
+                page.closeCard()
+
+            page.closeCard()
+
+    page.closeDiv()
+
+    html_file = os.path.join(curr_folder, "index.html")
+    page.savePage(html_file)
---
+++ tools/scattered_light.py
@@ -0,0 +1,126 @@
+#  scattered_light.py - this file is part of the gwadaptive_scattering package.
+#  Copyright (C) 2020- Stefano Bianchi
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+import os
+import numpy as np
+from ..utils import signal_utils, file_utils
+
+
+def scattered_light(gps, target_channel_name, channels_file, out_path, f_lowpass,
+                    fs=256, n_scattering=1, smooth_win=50):
+    """Analysis for scattered light identification.
+    The script outputs a folder named as the input gps,
+    with inside three files:
+        - target channel instantaneous amplitudes (*.imf)
+        - most correlated predictor for each imf (*.predictors)
+        - output.yml, a summary of the analysis' results
+
+    Parameters
+    ----------
+    gps : str
+        GPS start and end (comma separated)
+    target_channel_name : str
+        target channel name
+    channels_file : str
+        channels list
+    out_path : str
+        output path where to save results
+    f_lowpass : float
+        lowpass filter frequency
+    fs : float, optional
+        channels resample frequency (default : 256)
+    n_scattering : int, optional
+        number of signal bounces (default : 1)
+    smooth_win : int, optional
+        signals smoothing window (default : 50)
+    """
+    # initialize variables
+    ch_f = open(channels_file, "r")
+    channels_list = [ch.rstrip() for ch in ch_f.readlines() if ch.strip()]
+    ch_f.close()
+
+    # create folder for results if it does not exist
+    start_end = gps.split(",")
+    if len(start_end) != 2:
+        raise ValueError("GPS start or end time not provided.")
+
+    odir_name = "{}_{}".format(start_end[0], start_end[1])
+    out_path = os.path.join(out_path, odir_name)
+    if not os.path.isdir(out_path):
+        os.makedirs(out_path, exist_ok=True)
+        
+    # build time series matrix
+    gps_start = int(start_end[0])
+    gps_end = int(start_end[1])
+    data, fs = signal_utils.get_data_from_time_series_dict(target_channel_name, channels_list,
+                                                           gps_start, gps_end, fs, verbose=True)
+
+    # predictors
+    predictor = signal_utils.get_predictors(data[:, 1:], fs, smooth_win=smooth_win, n_scattering=n_scattering)
+
+    # target channel
+    target_channel = signal_utils.butter_lowpass_filter(data[:, 0], f_lowpass, fs)
+
+    # tvf-emd
+    imfs = signal_utils.get_imfs(target_channel, fs)
+
+    # correlations
+    corrs = np.zeros((imfs.shape[1], predictor.shape[1]), dtype=float)
+    envelopes = np.zeros((imfs.shape[0] - 1, imfs.shape[1]), dtype=float)
+    for k in range(imfs.shape[1]):
+        upper_env = signal_utils.upper_envelope(imfs[:, k])[1:]
+        upper_env = signal_utils.smooth(upper_env, smooth_win)
+        envelopes[:, k] = upper_env
+        for l in range(predictor.shape[1]):
+            corrs[k, l] = signal_utils.get_correlation_between(predictor[:, l], upper_env)
+
+    file_utils.save_envelopes(envelopes, "_".join(target_channel_name.split(":")), out_path)
+
+    # max correlations
+    max_vals = np.max(corrs, axis=1)
+    max_channels = np.argmax(corrs, axis=1)
+    max_channel = int(np.argmax(max_vals))
+    try:
+        max_ch_str = channels_list[max_channels[max_channel]]
+        mean_freq = signal_utils.mean_frequency(max_ch_str, gps_start, gps_end, bandpass_limits=(0.03, 10))
+    except:
+        max_ch_str = "Not found"
+        mean_freq = 0.0
+
+    # output file
+    out_file = file_utils.YmlFile()
+    out_file.write_parameters(gps, target_channel_name, channels_file, out_path,
+                              fs, f_lowpass, n_scattering, smooth_win)
+    out_file.write_max_corr_section(max_channel + 1, max_ch_str, max_vals[max_channel], mean_freq)
+    ch_str = []
+    ch_corr = []
+    ch_m_fr = []
+    for n_imf in range(len(max_vals)):
+        try:
+            ch_str.append(channels_list[max_channels[n_imf]])
+            ch_corr.append(max_vals[n_imf])
+            ch_m_fr.append(signal_utils.mean_frequency(channels_list[max_channels[n_imf]], gps_start,
+                                                       gps_end, bandpass_limits=(0.03, 10)))
+        except:
+            ch_str.append("Not found")
+            ch_corr.append(-999.0)
+            ch_m_fr.append(0.0)
+    out_file.write_correlation_section(ch_str, ch_corr, ch_m_fr)
+    out_file.save(out_path)
+
+    # save predictors
+    file_utils.save_predictors(predictor[:, max_channels], "_".join(target_channel_name.split(":")), out_path)
